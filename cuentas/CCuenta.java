cbdf5231e648b2116a2f54dd4ec26a8453452ed3
javierdiazsoto <javierds1310@gmail.com>
2025-02-11 17:25:36 +0100

    Primer commit: Proyecto CCuenta

---
+++ cuentas/CCuenta.java
@@ -0,0 +1,38 @@
+package cuentas;
+
+public class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta() {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        this.nombre = nom;
+        this.cuenta = cue;
+        this.saldo = sal;
+        this.tipoInterés = tipo;
+    }
+
+    public double estado() {
+        return saldo;
+    }
+
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0)
+            throw new Exception("No se puede retirar una cantidad negativa");
+        if (estado() < cantidad)
+            throw new Exception("No hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+}
